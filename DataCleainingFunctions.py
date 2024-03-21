484d3a2d8b9806088b5b55e0a274f5d06a11dce8
CrisMagdziak <162913111+CrisMagdziak@users.noreply.github.com>
2024-03-21 20:54:23 +0100

    Data Check

--- DataCleainingFunctions.py
+++ DataCleainingFunctions.py
@@ -1,4 +1,5 @@
 def stats(dataframe) :
+    """Returns informations about dataset - shape and data types"""
     print('### Shape ###')
     print(f'Rows: {dataframe.shape[0]}')
     print(f'Columns: {dataframe.shape[1]}')
@@ -6,8 +7,15 @@ def stats(dataframe) :
     print('### Data Types ###')
     print(dataframe.dtypes)
     print('\n')
+
+def miss_dupl(dataframe) :
+    """Return information about missing values and duplicates in dataset"""
+    print('\n')
     print('### Missing Values ###')
     print(dataframe.isna().sum())
     print('\n')
     print('### Duplicate Values ###')
     print(dataframe.duplicated().sum())
+    print('\n')
+    print('### Unique Values ###')
+    print(dataframe.nunique().sum())
